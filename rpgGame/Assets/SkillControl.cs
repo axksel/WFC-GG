cd51e60a451cbe014336554da7932d5e11b2079b
axksel <jonas.aksel@gmail.com>
2019-02-15 13:59:19 +0100

    weapons

--- rpgGame/Assets/SkillControl.cs
+++ rpgGame/Assets/SkillControl.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillControl : MonoBehaviour
 {
     public SkillScriptableObject equippedSkill;
     public GameObject EquippedPosition;
+    public GameObject button;
+    public float count=0;
+   
+
     void Start()
     {
         GameObject tmpSkill = Instantiate(equippedSkill.skillPrefab, EquippedPosition.transform.position, EquippedPosition.transform.rotation);
-        tmpSkill.transform.SetParent(gameObject.transform);
+        tmpSkill.transform.SetParent(EquippedPosition.transform);
+        button.GetComponentInChildren<Text>().text = equippedSkill.name;
+       
+ 
     }
+
+    private void Update()
+    {
+        
+        if (count<360)
+        {
+            EquippedPosition.transform.RotateAround(transform.position, Vector3.up, 1000 * Time.deltaTime);
+            count += 1000 * Time.deltaTime;
+        }
+     
+       
+
+    }
+
+    public void Attack()
+    {
+
+        count = 0;
+    }
+
 
    
 }
