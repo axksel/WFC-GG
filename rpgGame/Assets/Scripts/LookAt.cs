043d3a0d70988c0338d68302bb5390d3b364ee5d
Tobiasnm <tnma14@student.aau.dk>
2019-02-15 14:00:15 +0100

    controller

---
+++ rpgGame/Assets/Scripts/LookAt.cs
@@ -0,0 +1,46 @@
+﻿// LookAt.cs
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Animator))]
+public class LookAt : MonoBehaviour {
+	public Transform head = null;
+	public Vector3 lookAtTargetPosition;
+	public float lookAtCoolTime = 0.2f;
+	public float lookAtHeatTime = 0.2f;
+	public bool looking = true;
+
+	private Vector3 lookAtPosition;
+	private Animator anim;
+	private float lookAtWeight = 0.0f;
+
+	void Start ()
+	{
+		if (!head)
+		{
+			Debug.LogError("No head transform - LookAt disabled");
+			enabled = false;
+			return;
+		}
+		anim = GetComponent<Animator> ();
+		lookAtTargetPosition = head.position + transform.forward;
+		lookAtPosition = lookAtTargetPosition;
+	}
+
+	void OnAnimatorIK ()
+	{
+		lookAtTargetPosition.y = head.position.y;
+		float lookAtTargetWeight = looking ? 1.0f : 0.0f;
+
+		Vector3 curDir = lookAtPosition - head.position;
+		Vector3 futDir = lookAtTargetPosition - head.position;
+
+		curDir = Vector3.RotateTowards(curDir, futDir, 6.28f*Time.deltaTime, float.PositiveInfinity);
+		lookAtPosition = head.position + curDir;
+
+		float blendTime = lookAtTargetWeight > lookAtWeight ? lookAtHeatTime : lookAtCoolTime;
+		lookAtWeight = Mathf.MoveTowards (lookAtWeight, lookAtTargetWeight, Time.deltaTime/blendTime);
+		anim.SetLookAtWeight (lookAtWeight, 0.2f, 0.5f, 0.7f, 0.5f);
+		anim.SetLookAtPosition (lookAtPosition);
+	}
+}
--- rpgGame/Assets/Scripts/PlayerControl.cs
+++ rpgGame/Assets/Scripts/PlayerControl.cs
@@ -10,26 +10,58 @@ public class PlayerControl : MonoBehaviour
     private Vector2 dir;
 
     public float movementSpeed;
+    public GameObject ting;
 
     UnityEngine.AI.NavMeshAgent agent;
     //public NavMeshSurface[] surfaces;
 
+    Animator anim;
+    Vector2 smoothDeltaPosition = Vector2.zero;
+
+    RaycastHit hitInfo = new RaycastHit();
+
+    Vector2 velocity = Vector2.zero;
 
 
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-
-
-
+        anim = GetComponent<Animator>();
+        agent.updatePosition = false;
     }
 
-// Update is called once per frame
-void FixedUpdate()
+    private void Update()
     {
         dir = joystick.GetComponent<Joystick>().direction;
-        //transform.position += new Vector3(dir.x, 0, dir.y).normalized * 0.001f * Mathf.Clamp(dir.magnitude, 0, 30) * movementSpeed;
-        agent.destination = transform.position + new Vector3(dir.x, 0, dir.y).normalized;
+        agent.speed = Mathf.Clamp(dir.magnitude, 0, 30) / 8;
+        agent.destination = transform.position + new Vector3(dir.x, 0, dir.y).normalized / 5;
+        Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+
+        // Map 'worldDeltaPosition' to local space
+        float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+        float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
+        Vector2 deltaPosition = new Vector2(dx, dy);
+
+        // Low-pass filter the deltaMove
+        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+        smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
+
+        // Update velocity if time advances
+        if (Time.deltaTime > 1e-5f)
+            velocity = smoothDeltaPosition / Time.deltaTime;
+
+        bool shouldMove = velocity.magnitude > 0.5f /*&& agent.remainingDistance > agent.radius*/;
+
+        // Update animation parameters
+        anim.SetBool("shouldMove", shouldMove);
+        anim.SetFloat("velX", velocity.x);
+        anim.SetFloat("velY", velocity.y);
+
+        GetComponent<LookAt>().lookAtTargetPosition = agent.steeringTarget + transform.forward;
+    }
+
+    void FixedUpdate()
+    {
 
 
     }
@@ -45,5 +77,11 @@ void FixedUpdate()
         float _x = v.x * Mathf.Cos(radian) - v.y * Mathf.Sin(radian);
         float _y = v.x * Mathf.Sin(radian) + v.y * Mathf.Cos(radian);
         return new Vector2(_x, _y);
+    }
+
+    void OnAnimatorMove()
+    {
+        // Update position to agent position
+        transform.position = agent.nextPosition;
     }
 }
