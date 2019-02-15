69b20c3f3d5bfce0159c88e7dfed4bf34f95853f
axksel <jonas.aksel@gmail.com>
2019-02-15 11:47:24 +0100

    added skills

---
+++ rpgGame/Assets/Scripts/SkillScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "skill", menuName = "Skill", order = 1)]
+public class SkillScriptableObject : ScriptableObject
+{
+    public string objectName = "New MyScriptableObject";
+    public int dmg;
+    public GameObject skillPrefab;
+}
---
+++ rpgGame/Assets/SkillControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillControl : MonoBehaviour
+{
+    public SkillScriptableObject equippedSkill;
+    public GameObject EquippedPosition;
+    void Start()
+    {
+        GameObject tmpSkill = Instantiate(equippedSkill.skillPrefab, EquippedPosition.transform.position, EquippedPosition.transform.rotation);
+        tmpSkill.transform.SetParent(gameObject.transform);
+    }
+
+   
+}
