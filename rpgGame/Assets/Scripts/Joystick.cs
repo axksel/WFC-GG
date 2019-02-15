17737619d5b911217b06b51ba044abb8e92f193a
Tobiasnm <tnma14@student.aau.dk>
2019-02-15 10:40:24 +0100

    movement

---
+++ rpgGame/Assets/Scripts/Joystick.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Joystick : MonoBehaviour
+{
+
+    public GameObject joystickOuter;
+    public GameObject joystickInner;
+    public Vector2 direction;
+    private float radius;
+    bool joystickPressed = false;
+
+    private float targetAlpha;
+    public float FadeRate;
+
+    private int screenWidth;
+    private int screenHeight;
+
+
+    void Start()
+    {
+        radius = joystickInner.GetComponentInParent<Canvas>().scaleFactor * 40;
+    }
+
+    void Update()
+    {
+        Color curColor = joystickInner.GetComponent<Image>().color;
+        float alphaDiff = Mathf.Abs(curColor.a - this.targetAlpha);
+        if (alphaDiff > 0.0001f)
+        {
+            curColor.a = Mathf.Lerp(curColor.a, targetAlpha, this.FadeRate * Time.deltaTime);
+            joystickInner.GetComponent<Image>().color = curColor;
+        }
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0) && Vector2.Distance(Input.mousePosition, joystickOuter.transform.position) < 50)
+        {
+            direction = Input.mousePosition - joystickOuter.transform.position;
+            joystickPressed = true;
+            FadeIn();
+        }
+
+        if (Input.GetMouseButton(0) && joystickPressed)
+        {
+            direction = Input.mousePosition - joystickOuter.transform.position;          
+            joystickInner.transform.position = joystickOuter.transform.position + Vector3.ClampMagnitude(direction, radius);
+        }
+
+        if (Input.GetMouseButtonUp(0) && joystickPressed)
+        {
+            joystickPressed = false;
+            direction = Vector2.zero;
+            FadeOut();
+        }
+
+#endif
+
+#if UNITY_ANDROID
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+
+                    if(Vector2.Distance(touch.position, joystickOuter.transform.position) < 50){
+                        joystickInner.SetActive(true);
+                        direction = touch.position - new Vector2(joystickOuter.transform.position.x, joystickOuter.transform.position.y);
+                        joystickPressed = true;
+                        FadeIn();
+                    }
+                    break;
+
+                case TouchPhase.Moved:
+                    if (joystickPressed)
+                    {
+                        direction = touch.position - new Vector2(joystickOuter.transform.position.x, joystickOuter.transform.position.y);
+                        joystickInner.transform.position = joystickOuter.transform.position + Vector3.ClampMagnitude(direction, radius);
+                    }
+                    break;
+
+                case TouchPhase.Ended:
+                    if (joystickPressed)
+                    {
+                        joystickInner.SetActive(false);
+                        direction = Vector2.zero;
+                        FadeOut();
+                    }
+                    break;
+            }
+        }
+
+#endif
+    }
+
+    public void FadeOut()
+    {
+        this.targetAlpha = 0.0f;
+    }
+
+    public void FadeIn()
+    {
+        this.targetAlpha = 1.0f;
+    }
+}
---
+++ rpgGame/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public GameObject joystick;
+    private Vector2 dir;
+
+    public float movementSpeed;
+
+    UnityEngine.AI.NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        dir = joystick.GetComponent<Joystick>().direction;
+        //transform.position += new Vector3(dir.x, 0, dir.y).normalized * 0.001f * Mathf.Clamp(dir.magnitude, 0, 30) * movementSpeed;
+        agent.destination = transform.position + new Vector3(dir.x, 0, dir.y).normalized;
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+    public Vector2 RotateVector(Vector2 v, float angle)
+    {
+        float radian = angle * Mathf.Deg2Rad;
+        float _x = v.x * Mathf.Cos(radian) - v.y * Mathf.Sin(radian);
+        float _y = v.x * Mathf.Sin(radian) + v.y * Mathf.Cos(radian);
+        return new Vector2(_x, _y);
+    }
+}
