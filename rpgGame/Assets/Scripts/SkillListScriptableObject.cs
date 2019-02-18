ec6a6bbdb9f546844974b4be784fa4df43790aa3
axksel <jonas.aksel@gmail.com>
2019-02-18 14:56:40 +0100

    shop commit

---
+++ rpgGame/Assets/Scripts/SkillListScriptableObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "skilllist", menuName = "SkillList", order = 1)]
+public class SkillListScriptableObject : ScriptableObject
+{
+   public  List<SkillScriptableObject> list = new List<SkillScriptableObject>();
+
+}
--- rpgGame/Assets/slotMachineScript.cs
+++ rpgGame/Assets/slotMachineScript.cs
@@ -1,23 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class slotMachineScript : MonoBehaviour
 {
     public SkillScriptableObject skill;
+    public SkillListScriptableObject soldSKill;
     public intScriptableObject playerGold;
+    public UnityEvent weaponBought;
+    public bool bob = false;
     void Start()
     {
-       Instantiate(skill.skillPrefab, transform.position, Quaternion.identity);
-        skill.isLoot = true; 
+      // Instantiate(skill.skillPrefab, transform.position, Quaternion.identity);
+        skill.isLoot = true;
+        soldSKill.list.Clear();
 
+    }
+
+    private void Update()
+    {
+        if (bob)
+        {
+            Interact();
+            bob = false;
+        }
     }
 
     public void Interact()
     {
         if (playerGold.value >= skill.price)
         {
-
+            soldSKill.list.Clear();
+            skill.isLoot = false;
+            soldSKill.list.Add(skill);
+            playerGold.value = playerGold.value - skill.price;
+            weaponBought.Invoke();
+            
+            
 
         }
 
