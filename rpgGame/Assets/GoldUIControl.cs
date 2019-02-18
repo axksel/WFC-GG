c52829f84195a464a0b7db7c1cc5b9be1a4f5f1f
axksel <jonas.aksel@gmail.com>
2019-02-18 16:06:28 +0100

    tiøfæhet guld ui

---
+++ rpgGame/Assets/GoldUIControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldUIControl : MonoBehaviour
+{
+
+    public intScriptableObject gold;
+    Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Gold :" + gold.value;
+    }
+}
