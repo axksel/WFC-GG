b93753ab15f2e83324dc676df27b07985178d619
axksel <jonas.aksel@gmail.com>
2019-02-21 19:15:10 +0100

    door and key

---
+++ rpgGame/Assets/DoorwayScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorwayScript : MonoBehaviour,IsInteracable
+{
+
+    public GameObject door;
+    public EnemyList inventory;
+   public string ReturnName()
+    {
+        return "Unlock door with key";
+
+    }
+
+  public void Interact()
+    {
+        foreach (var item in inventory.list)
+        {
+            if(item.name == "key")
+            {
+                inventory.list.Remove(item);
+                Destroy(door);
+            }
+        }
+
+    }
+}
---
+++ rpgGame/Assets/KeyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour,IsInteracable
+{
+    public EnemyList inventory;
+    public string ReturnName()
+    {
+
+        return "Pick up key";
+    }
+
+    public void Interact()
+    {
+        inventory.list.Add(this.gameObject);
+        gameObject.SetActive(false);
+
+    }
+
+}
