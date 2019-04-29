819fbf3ad8d06e3b66f575a55b17f951d0508c81
Tobiasnm <tnma14@student.aau.dk>
2019-04-29 14:19:18 +0200

    mailsender

---
+++ rpgGame/Assets/MailSender.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class mono_gmail : MonoBehaviour
+{
+
+    void Main()
+    {
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress("youraddress@gmail.com");
+        mail.To.Add("youraddress@gmail.com");
+        mail.Subject = "Test Mail";
+        mail.Body = "This is for testing SMTP mail from GMAIL";
+
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 587;
+        smtpServer.Credentials = new System.Net.NetworkCredential("youraddress@gmail.com", "yourpassword") as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback =
+            delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            { return true; };
+        smtpServer.Send(mail);
+        Debug.Log("success");
+
+    }
+}
